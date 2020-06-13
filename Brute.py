0787f7d7758baf4c256ee963aa490af862c4ce02
kds <koutsenko.work@gmail.com>
2020-06-13 16:30:44 +0300

    Make separate thread for each site

--- Brute.py
+++ Brute.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import requests
+from threading import Thread
 
 sites = [
     'http://www.google.com',
@@ -9,9 +10,13 @@ sites = [
     'http://fb.com',
     'http://www.gmail.com',
 ]
+reqs_per_site = 100
 
-for tryout in range(100):
-    for site in sites:
+def request(site, count):
+    for tryout in range(1, count+1):
         response = requests.get(site)
         print('№', tryout, 'GET', site, response.status_code, len(response.text), 'bytes')
 
+
+for site in sites:
+    Thread(target=request, args=(site, reqs_per_site)).start()
